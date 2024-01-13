e3f05f0503ee9f13aff87d8d246a4eb91fde34f0
HNTech <m.hamza.naveed@outlook.com>
2024-01-13 12:17:07 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+from setuptools import setup
+
+setup(
+    name="TKImageButton",
+    version="0.1.0",
+    description="A Python library for creating image buttons with Tkinter.",
+    author="Hamza Naveed",
+    author_email="m.hamza.naveed@outlook.com",
+    url="https://github.com/hntechsoftware/TKImageButton",
+    packages=["TKImageButton"],
+    install_requires=["tkinter"],
+)
